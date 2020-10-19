5e345f0c7ee56d85528b8ff3f4f077bc6923f4ca
Sun-droid <colour1@protonmail.com>
2020-10-19 13:55:43 +0200

    Pickers-layout

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,6 +6,23 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_set_pickers)
+        /*goToCathegories.setOnClickLister{
+           setContentView(R.layout.activity_set_pickers)
+        }*/
+
+        // get reference to button
+        /*val btn_click_me = findViewById(R.id.searchCathegories) as Button
+        // set on-click listener
+        btn_click_me.setOnClickListener {
+            // your code to perform when the user clicks on the button
+            Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
+        }*/
+
+
     }
 }
+
+
+
